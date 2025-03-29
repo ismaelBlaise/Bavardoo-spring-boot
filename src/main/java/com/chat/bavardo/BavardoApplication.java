ce8584adaffd1140bb7313fbdc72e4482f4bb0a4
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-29 10:21:31 +0300

    commit

---
+++ src/main/java/com/chat/bavardo/BavardoApplication.java
@@ -0,0 +1,13 @@
+package com.chat.bavardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BavardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BavardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/bavardo/BavardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.bavardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BavardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
